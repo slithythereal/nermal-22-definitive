2096d3d6dc0d2455d69b917c2ef1136fd8e0d853
slithythereal <williamrunion1@gmail.com>
2026-07-31 17:14:13 -0400

    notes done

--- data/notes/jumpScareNote.hx
+++ data/notes/jumpScareNote.hx
@@ -1,13 +1,19 @@
 importScript('data/scripts/HandyDandy.hx');
 
-function create(){
-
-}
-
 function onPlayerHit(event){
     if(event.noteType == 'jumpScareNote'){
-        //show scary garfield\
-        health -= 0.18;
+        health -= 1;
+        var garf:FlxSprite = new FlxSprite(50, 0);
+        garf.loadGraphic(Paths.image('game/mech/scarygarfield'));
+        garf.scale.set(1.8, 1);
+        garf.updateHitbox();
+        garf.cameras = [camHUD];
+        garf.alpha = 1;
+        add(garf);
+        new FlxTimer().start(0.5, function(tmr:FlxTimer){
+            FlxTween.tween(garf, {alpha: 0}, 0.5, {ease:FlxEase.linear});
+        });
+        FlxG.sound.play(Paths.sound('sonic.exe laugh'), 0.5);
     }
 }
 
--- data/notes/nermalNote.hx
+++ data/notes/nermalNote.hx
@@ -1,29 +1,70 @@
 importScript('data/scripts/HandyDandy.hx');
+var nermB:FlxSprite;
+var nermT:FlxSprite;
+var customScare:String = null;
 
-function create(){
+var customScareMap = [
+	'garfield' => {sound: "fard", noteSkin: 'GARFNOTES', jumpScare: 'garfield jumpscare'}
+];
 
+function create() {
+	if (PlayState.SONG.meta.customValues?.nermalNoteType != null)
+		customScare = PlayState.SONG.meta.customValues?.nermalNoteType;
+
+	nermB = new FlxSprite(-400, 500);
+	add(nermB);
+	nermT = new FlxSprite(-400, -570);
+	add(nermT);
+	for (nermal in [nermB, nermT]) {
+		nermal.loadGraphic(Paths.image('game/mech/nermal jumpscare'));
+		if (customScare != null && customScareMap.exists(customScare))
+			if (customScareMap[customScare].jumpScare != null)
+				nermal.loadGraphic(Paths.image('game/mech/' + customScareMap[customScare].jumpScare));
+		nermal.scale.set(2, 0.5);
+		nermal.updateHitbox();
+		nermal.cameras = [camHUD];
+		nermal.alpha = 0.001;
+	}
+	nermT.flipY = true;
 }
 
-function onPlayerHit(event){
-    if(event.noteType == 'nermalNote'){
-        //make nermal block hud\
-        health -= 0.18;
-    }
+function onPlayerHit(event) {
+	if (event.noteType == 'nermalNote') {
+		health -= 0.18;
+
+		var soundID:String = 'wow';
+		if (customScare != null)
+			if (customScareMap[customScare].sound != null)
+				soundID = customScareMap[customScare].sound;
+		FlxG.sound.play(Paths.sound(soundID), 1);
+
+		camGame.shake(0.10, 0.5);
+		camHUD.shake(0.10, 0.5);
+
+		for (i in [nermB, nermT]) {
+			FlxTween.tween(i, {alpha: 1}, 1, {ease: FlxEase.bounceOut});
+			new FlxTimer().start(7, function(tmr:FlxTimer) {
+				FlxTween.tween(i, {alpha: 0.001}, 1, {ease: FlxEase.bounceOut});
+			});
+		}
+	}
 }
 
-function onPlayerMiss(event){
-    if(event.noteType == 'nermalNote'){
-        event.animCancelled = true;
-        event.cancel(true);
-        event.cancelResetCombo();
-        var note:Note = event.note;
-        remove(note);
-        note.destroy();
-    }
+function onPlayerMiss(event) {
+	if (event.noteType == 'nermalNote') {
+		event.animCancelled = true;
+		event.cancel(true);
+		event.cancelResetCombo();
+		var note:Note = event.note;
+		remove(note);
+		note.destroy();
+	}
 }
 
-function onNoteCreation(event)
-{
-    if(event.noteType == 'nermalNote' && PlayState.SONG.meta.customValues?.nermalNoteType == 'garfield')
-        event.noteSprite = 'game/notes/GARFNOTES';
+function onNoteCreation(event) {
+	if (event.noteType == 'nermalNote' && customScare != null) {
+		if (customScareMap.exists(customScare)) {
+			event.noteSprite = 'game/notes/' + customScareMap[customScare].noteSkin;
+		}
+	}
 }
