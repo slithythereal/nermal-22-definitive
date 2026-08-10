1faa2a0f5af3b53a016449a88af999ac285bddde
slithythereal <williamrunion1@gmail.com>
2026-08-09 21:13:40 -0400

    sum popout fixes

--- data/stages/funko.hx
+++ data/stages/funko.hx
@@ -1,15 +1,16 @@
 var playerStrumPos:Array<Float> = [300, 428, 732, 860];
-function postCreate() {
-    dad.visible = false; //WHY DOES THIS WORK
 
-    for(i in [iconP1, iconP2, healthBarBG, healthBar, scoreTxt, accuracyTxt, missesTxt])
-        i.visible = false;
-	for (i => strumLine in strumLines.members) //...ig...
+function postCreate() {
+	dad.visible = false; // WHY DOES THIS WORK
+
+	for (i in [iconP1, iconP2, healthBarBG, healthBar/*, scoreTxt, accuracyTxt, missesTxt*/])
+		i.visible = false;
+	for (i => strumLine in strumLines.members) // ...ig...
 		if (i == 0)
 			for (i in 0...4)
 				strumLine.members[i].x = playerStrumPos[i];
-
 }
-function update(elapsed:Float){
-    health = 2; //this was what was in the og script so yea u cant die in popout :man_shrugging:
+
+function update(elapsed:Float) {
+	health = 2; // this was what was in the og script so yea u cant die in popout :man_shrugging:
 }
