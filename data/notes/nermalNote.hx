95168f0ab667c3e43a331db45be84a7abe28da6e
slithythereal <williamrunion1@gmail.com>
2026-08-26 16:23:52 -0400

    byeclutter

--- data/notes/nermalNote.hx
+++ data/notes/nermalNote.hx
@@ -44,7 +44,6 @@ function create() {
 	}
 }
 
-// var timers:Array<FlxTimer> = [];
 var wallTimer:FlxTimer;
 
 function onPlayerHit(event) {
@@ -82,66 +81,13 @@ function onPlayerMiss(event) {
 	}
 }
 
-/*
-	function postUpdate(e:Float) {
-	for (strumLine in strumLines.members) {
-		for (note in strumLine.notes) {
-			//trace(note.noteType);
-			if(note.noteType != 'nermalNote')
-				return;
-			//note.angle = Math.random(0.0, 0.9);
-			note.x = note.y / -1.1 * Math.sin(Conductor.songPosition / 100) * 0.3;
-			//note.offset.x = note.y * Math.sin(Conductor.songPosition / 100) * 0.3;
-		}
-	}
-}*/
+
 function onNoteUpdate(e:NoteUpdateEvent) {
 	var note:Note = e.note;
 	if (note.noteType != "nermalNote")
 		return;
-
-	/*trace(note.offset.x);
-		note.updateNotesPosX = false;
-		var startWindow:Float = hitWindow * 0.5 + 255;
-
-		var ogNote:Float = note.offset.x;
-		var timeUntilNote:Float = note.strumTime - Conductor.songPosition;
-		note.offset.x = note.y * Math.sin(Conductor.songPosition / 100) * 0.3;
-
-		FlxTween.num(note.offset.x, ogNote, timeUntilNote, {type:FlxTween.PINGPONG, ease:FlxEase.linear});
-	 */
-	// var curX:Float = note.x;
-
-	// note.y = note.y / -1 * Math.abs(Math.sin(Conductor.songPosition / 100 * 1));
-	/**
-		* 
-							if dadName == 'garfield' then
-								setPropertyFromGroup('notes', i, 'offsetY', getPropertyFromGroup('notes', i, 'y') / -1 * math.abs(math.sin(getPropertyFromClass('Conductor', 'songPosition') / 100) * 1))
-							else
-								setPropertyFromGroup('notes', i, 'offsetY', getPropertyFromGroup('notes', i, 'y') / -1.1)
-							end
-							
-							setPropertyFromGroup('notes', i, 'offsetX', getPropertyFromGroup('notes', i, 'y') * math.sin(getPropertyFromClass('Conductor', 'songPosition') / 100) * 0.3)
-	 */
-	/*var strums:Array<Strum> = [
-			strumLines.members[0].members[note.noteData],
-			strumLines.members[1].members[note.noteData]
-		];
-		note.x = note.y * Math.sin(Conductor.songPosition / 100 * 0.3);
-		note.offset.y = note.y / -1 * Math.abs(Math.sin(Conductor.songPosition / 100 * 1)); */
 }
 
-/*function postUpdate(e:Float) {
-	for (strumLine in strumLines.members) {
-		for (note in strumLine.notes) {
-			if (note.noteType == 'nermalNote') {
-				//modchart.ease('bals', PlayState.instance.curBeat, 5, 1, 1, 1);
-				//note.offset.y = note.y / -1 * Math.abs(Math.sin(Conductor.songPosition / 100 * 1));
-				//note.offset.x = note.y * Math.sin(Conductor.songPosition / 100) * 0.3;
-			}
-		}
-	}
-}*/
 function onNoteCreation(event) {
 	if (event.noteType == 'nermalNote') {
 		if (customScare != null && FlxG.save.data.customNermalNotes)
@@ -154,51 +100,8 @@ function onNoteCreation(event) {
 		}
 	}
 }
-/*function postUpdate(elapsed:Float) {
-	for (note in strumLines.notes) {
-		if (note.noteType == 'nermalNote') {
-			note.updateNotesPosY = true;
-			note.updateNotesPosX = true;
-			note.offset.y = note.y / -1 * Math.abs(Math.sin(Conductor.songPosition / 100 * 1));
-			note.offset.x = note.y * Math.sin(Conductor.songPosition / 100) * 0.3;
-		}
-	}
-}*/
 /*
-	function onNoteUpdate(e:NoteUpdateEvent) {
-	var note:Note = e.note;
-
-	if (note.noteType == 'nermalNote' && PlayState.SONG.meta.customValues?.noteSwing == 'true') {
-		note.updateNotesPosY = true;
-		note.updateNotesPosX = true;
-	//	setPropertyFromGroup('notes', i, 'offsetY', getPropertyFromGroup('notes', i, 'y') / -1 * math.abs(math.sin(getPropertyFromClass('Conductor', 'songPosition') / 100) * 1))
-
-		
-		///note.y = note.y / -1 * Math.abs(Math.sin(Conductor.songPosition / 100 * 1));
-		///note.x = note.y * Math.sin(Conductor.songPosition / 100) * 0.3;
-	}
-}*/
-/*
-	function onNoteUpdate(e:NoteUpdateEvent) {
-	if (FlxG.save.data.noteSwing && !FlxG.save.data.pussyMode) {
-		var note:Note = e.note;
-		trace("works");
-
-		if (note.noteType != "nermalNote")
-			return;
-
-		
-			if(PlayState.SONG.meta.customValues?.noteSwing == 'true'){
-
-				//note.y = note.y / -1.1;
-
-				note.x = note.y * Math.sin(Conductor.songPosition / 100) * 3;
-					//	setPropertyFromGroup('notes', i, 'offsetX', getPropertyFromGroup('notes', i, 'y') * math.sin(getPropertyFromClass('Conductor', 'songPosition') / 100) * 0.3)
-
-		}
-	}
-}*/
-/*
+	GAY NOTE SWING CODE WRITTEN BY CAR (`.lua` format)
 	* function onUpdate(elapsed)
 		--note stuff
 		for i = 0, getProperty('notes.length')-1 do
